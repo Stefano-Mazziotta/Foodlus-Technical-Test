bd11bf2480c4afb21340f7a118883b9414096842
stefano-mazziotta <mazziottastefano@gmail.com>
2024-02-05 16:32:56 -0300

    rename userExtrator.ts to user-extractor.ts

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -2,7 +2,7 @@ import revoRoutes from "../modules/revo/infrastructure/route/revo.route"
 import authRoutes from "../modules/auth/infrastructure/routes/auth.route"
 
 import { Router } from "express"
-import { userExtractor } from "../modules/auth/infrastructure/middlewares/userExtractor"
+import { userExtractor } from "../modules/auth/infrastructure/middlewares/user-extractor"
 
 export const routes = Router()
 
